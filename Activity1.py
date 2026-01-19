05767c4858ea1d69c2eb2cabc643d47de42fa1ca
Aayansh889 <aayansh.saxena@gmail.com>
2026-01-19 18:20:29 +0530

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,14 @@
+tuplex=("tuple",False,3.2,9)
+print(tuplex)
+
+tuple=(4,6,2,8,1)
+tuple=tuple+(9,)
+print(tuple)
+
+tuple1=(50,70,50,60,90)
+print(tuple1.count(50))
+
+tupley=(2,3,4,5,1,3,2,9)
+slice=tupley[3:5]
+slice_=tupley[:6]
+print(slice,"\n",slice_)
